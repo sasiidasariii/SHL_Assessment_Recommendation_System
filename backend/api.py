ed64a5ae20eb3620154404b825aadf13bed20c1d
Sasiidasariii <sasirangaraidu555@gmail.com>
2025-04-05 18:15:15 +0530

    new

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from models.recommender import SHLRecommender
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 app = FastAPI()
 recommender = SHLRecommender()
@@ -12,3 +15,5 @@ class QueryRequest(BaseModel):
 def recommend_tests(request: QueryRequest):
     results = recommender.recommend(request.query)
     return {"results": results}
+if __name__ == "__main__":
+    app.run(debug=True)
